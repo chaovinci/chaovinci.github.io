8c8680026445b283c7d42617c4287664ff5c08f1
Vinci Chao <chaovinci@gmail.com>
2020-08-04 22:42:28 +0800

    Update snake.js

--- images/snake.js
+++ images/snake.js
@@ -1,10 +1,9 @@
 function Snake() {
 
   this.initialization = function() {
-    this.speed = createVector(1,0);
+    this.speed = createVector(0,0);
     this.body = [];
     this.body[0] = createVector(10, 50);
-    this.grow();
   }
 
 
